6d5b8a56acedd3fa60a77bee189c71d1c5e17d59
= <andy_akins@mac.com>
2020-12-03 19:52:29 -0500

    adding security group

---
+++ inputs.tf
@@ -0,0 +1,4 @@
+variable "deployment_name" {
+  description = ""
+  type = string
+}
---
+++ security_group.tf
@@ -0,0 +1,56 @@
+resource "aws_security_group" "FoundryServerSG" {
+  name        = "${var.deployment_name}-FoundryServerSG"
+  description = "Allow Traffic to and from foundry server"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "-"
+    from_port   = 30000
+    to_port     = 30000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP inbound"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS inbound"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Voice inbound"
+    from_port   = 49152
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Foundry" inbound"
+    from_port   = 33478
+    to_port     = 33478
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
