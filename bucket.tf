4666aa4a17a9a45bc9460c1296b0b873f8bbdf82
= <andy_akins@mac.com>
2020-12-03 21:22:39 -0500

    added bucket

---
+++ bucket.tf
@@ -0,0 +1,15 @@
+resource "aws_s3_bucket" "FoundryBucket" {
+  bucket = "${var.deployment_name}.foundry_bucket"
+  acl    = "public-read"
+
+  cors_rule {
+      allowed_headers = ["*"]
+      allowed_methods = ["GET", "PUT", "HEAD"]
+      allowed_origins = ["*"]
+      max_age_seconds = 3000
+  }
+
+  tags = {
+    Name        = "${var.deployment_name}.foundry_bucket"
+  }
+}
